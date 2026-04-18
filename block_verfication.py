93fe5630f98c9f5b1bcb76d2027cb2da03f29456
ASHRAAF <130046079+ironclad-1@users.noreply.github.com>
2026-04-18 09:36:02 +0530

    Update block_verfication.py
    Api key issue

--- block_verfication.py
+++ block_verfication.py
@@ -1,6 +1,6 @@
 import requests
 
-API_KEY = 'e1fbff21babcfcf72cc1b5ad2153f016c15fef1a02b0a48194946574ad0f1b63'
+API_KEY = ''
 TX_HASH = '220dac7a090c75c6b13575a886295b89de954d4a52a6713e249f84621203a458'
 
 url = f'https://api-sepolia.etherscan.io/api?module=proxy&action=eth_getTransactionByHash&txhash={TX_HASH}&apikey={API_KEY}'
